36e40c799b96bf9f7f23b715b33f591eec1f6f82
Samuel Dunesme <samuel.dunesme@ens-lyon.fr>
2024-12-17 14:38:53 +0100

    Sequencing

--- fct/workflows/disaggregation/Sequencing.py
+++ fct/workflows/disaggregation/Sequencing.py
@@ -13,21 +13,72 @@ Sequencing
 ***************************************************************************
 """
 
-import os
+import processing
 
-from qgis.core import ( 
-    QgsProcessingModelAlgorithm
+from qgis.core import (
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterFeatureSource,
+    QgsProcessingParameterVectorDestination,
+    QgsProcessingParameterEnum,
+    QgsProcessingParameterRasterLayer,
 )
 
 from ..metadata import AlgorithmMetadata
+from ...utils.assertions import assertLayersCompatibility
 
-class Sequencing(AlgorithmMetadata, QgsProcessingModelAlgorithm):
-    """ 
-    Sequencing
-    """
+class Sequencing(AlgorithmMetadata, QgsProcessingAlgorithm):
 
-    def __init__(self, *args, **kwargs):
+    METADATA = AlgorithmMetadata.read(__file__, 'Sequencing')
 
-        super().__init__(*args, **kwargs)
-        self.METADATA = AlgorithmMetadata.read(__file__, type(self).__name__)
-        self.fromFile(os.path.join(os.path.dirname(__file__), type(self).__name__ + '.model3'))
+    INPUT = 'INPUT'
+    DEM = 'DEM'
+    OUTPUT = 'OUTPUT'
+
+
+    def initAlgorithm(self, configuration): 
+
+        self.addParameter(QgsProcessingParameterFeatureSource(
+            self.INPUT,
+            self.tr('Input network'),
+            [QgsProcessing.TypeVectorLine]))
+        
+        self.addParameter(QgsProcessingParameterRasterLayer(
+            self.DEM,
+            self.tr('Raw DEM'),
+            optional=True))
+        
+        self.addParameter(QgsProcessingParameterVectorDestination(
+            self.OUTPUT,
+            self.tr('Sequenced network'),
+            QgsProcessing.TypeVectorLine))
+        
+
+    def processAlgorithm(self, parameters, context, feedback): 
+
+        network = self.parameterAsVectorLayer(parameters, self.INPUT, context)
+        dem = self.parameterAsRasterLayer(parameters, self.DEM, context)
+
+        assertLayersCompatibility([network, dem], feedback)
+
+        identify = processing.run('fct:identifynetworknodes', {
+            'INPUT': network,
+            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT,
+            'NODES': QgsProcessing.TEMPORARY_OUTPUT,
+        }, context=context, feedback=feedback, is_child_algorithm=True)
+
+        drape = processing.run('native:setzfromraster', {
+            'INPUT': identify['NODES'],
+            'RASTER': dem,
+            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT,
+        }, context=context, feedback=feedback, is_child_algorithm=True)
+
+        oriented = processing.run('fct:fixlinkorientation', {
+            'INPUT': identify['OUTPUT'],
+            'NODES': drape['OUTPUT'],
+            'OUTPUT': parameters[self.OUTPUT],
+        }, context=context, feedback=feedback, is_child_algorithm=True)
+
+        return {
+            self.OUTPUT: oriented['OUTPUT'],
+        }
